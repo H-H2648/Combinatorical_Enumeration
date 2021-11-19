c5cd8bb77e747e65b38ff2f1366b8c4c84faf630
H-H2648 <sz2hong@uwaterloo.ca>
2021-11-18 22:08:38 -0800

    A7Q2

---
+++ Random Generation/A7Q2.py
@@ -0,0 +1,68 @@
+
+import random
+def binary0(v):
+    return "0"
+
+def binary1(v):
+    return "1"
+
+def binaryEmpty(v):
+    return ""
+
+##A, B refers to the generating function
+def generateAdd(A, B, gammaA, gammaB, v):
+    total = A(v) + B(v)
+    rand = random.random()
+    if rand < A(v)/(total):
+        return gammaA(v)
+    else:
+        return gammaB(v)
+
+def generateMult(gammaA, gammaB, v):
+    return gammaA(v) + gammaB(v)
+
+##A refers to the generating function
+def generateSEQ( A, gammaA, v):
+    rand = random.random()
+    k = 0
+    Av = A(v)
+    add_val = 1-Av
+    S = add_val
+    while S < rand:
+        k +=1
+        add_val *= Av
+        S += add_val
+    output = ""
+    for _ in range(k):
+        output += gammaA(v)
+    return output
+
+###-1 refers to error
+def main(v):
+    radius = (5**(1/2)-1)/2
+    if v < 0 or v > radius:
+        return -1
+    # def block_genF(v):
+    #     return 1/(1-v)
+    def eps_genF(v):
+        return 1
+    def atom_genF(v):
+        return v
+    def generateSEQ1(v):
+        return generateSEQ( atom_genF, binary1, v)
+    SEQ_1 = generateSEQ1(v)
+    def generateSEQ1_with1(v):
+        return generateMult(binary1, generateSEQ1, v)
+    def generateSEQ1_with01(v):
+        return generateMult(binary0, generateSEQ1_with1, v)
+    def generateSEQ_SEQ1_with_01(v):
+        def generating_func(x):
+            return x**2/(1-x)
+        return generateSEQ(generating_func, generateSEQ1_with01, v)
+    MAIN_BLOCKS = generateSEQ_SEQ1_with_01(v)
+    FINAL = generateAdd(eps_genF, atom_genF, binaryEmpty, binary0, v)
+    return SEQ_1 + MAIN_BLOCKS + FINAL
+
+
+print(main(0.6))
+
